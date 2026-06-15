6c513c9a5e1a8bd8a58e4c92c8926ac5382130d3
Ironhammer Std <2482911962@qq.com>
2026-06-15 14:41:54 +0800

    Update Version Info

--- Version.h
+++ Version.h
@@ -3,33 +3,44 @@
 #define VMAJOR 0
 #define VMINOR 3
 #define VRELEASE 0
-#define VBUILD 13
+#define VBUILD 14
 
 #define PRODUCT_VERSION VMAJOR, VMINOR, VRELEASE, VBUILD
 #define FILE_VERSION VMAJOR, VMINOR, VRELEASE, VBUILD
-#define PRODUCT_VERSION_STR Str(VMAJOR) "." Str(VMINOR) "." Str(VRELEASE) "." Str(VBUILD)
-#define FILE_VERSION_STR Str(VMAJOR) "." Str(VMINOR) "." Str(VRELEASE) "." Str(VBUILD)
 
 #define StrImpl(x) #x
 #define Str(x) StrImpl(x)
 
-#if VBUILD == 0
-	#if VRELEASE == 0
-		#define VERSION_STR  Str(VMAJOR) "." Str(VMINOR)
-	#else
-		#define VERSION_STR  Str(VMAJOR) "." Str(VMINOR) "." Str(VRELEASE)
-	#endif
+#ifdef CUSTOM_VERSION_STR
+	#define VERSION_STR CUSTOM_VERSION_STR
+	constexpr auto const VersionString = "SyringeIH " VERSION_STR;
+	constexpr auto const VersionLString = L"SyringeIH " VERSION_STR;
+	#define PRODUCT_VERSION_STR CUSTOM_VERSION_STR
+	#define FILE_VERSION_STR CUSTOM_VERSION_STR
 #else
-	#if VRELEASE == 0
-		#define VERSION_STR  Str(VMAJOR) "." Str(VMINOR) "b" Str(VBUILD)
+	#if VBUILD == 0
+		#if VRELEASE == 0
+			#define VERSION_STR  Str(VMAJOR) "." Str(VMINOR)
+		#else
+			#define VERSION_STR  Str(VMAJOR) "." Str(VMINOR) "." Str(VRELEASE)
+		#endif
 	#else
-		#define VERSION_STR  Str(VMAJOR) "." Str(VMINOR) "." Str(VRELEASE) "b" Str(VBUILD)
+		#if VRELEASE == 0
+			#define VERSION_STR  Str(VMAJOR) "." Str(VMINOR) "b" Str(VBUILD)
+		#else
+			#define VERSION_STR  Str(VMAJOR) "." Str(VMINOR) "." Str(VRELEASE) "b" Str(VBUILD)
+		#endif
 	#endif
+	constexpr auto const VersionString = "SyringeIH " VERSION_STR;
+	constexpr auto const VersionLString = L"SyringeIH " VERSION_STR;
+	#define PRODUCT_VERSION_STR Str(VMAJOR) "." Str(VMINOR) "." Str(VRELEASE) "." Str(VBUILD)
+	#define FILE_VERSION_STR Str(VMAJOR) "." Str(VMINOR) "." Str(VRELEASE) "." Str(VBUILD)
 #endif
 
 
-constexpr auto const VersionString = "SyringeIH " VERSION_STR;
-constexpr auto const VersionLString = L"SyringeIH " VERSION_STR;
+
+
+
 const int VMajor = VMAJOR;
 const int VMinor = VMINOR;
 const int VRelease = VRELEASE;
